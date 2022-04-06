b35c30eb07a4669c2fcea051cbea3799091e3c98
Jean-Paul LE <jeanpaul.le@protonmail.com>
2022-04-06 22:58:45 +0200

    add file

---
+++ setup_env.sh
@@ -0,0 +1,36 @@
+# Create custom conda image
+docker build -f containers/conda/conda.dockerfile -t custom_conda .
+
+# Create network
+docker network create -d bridge dashnet
+
+# Create containers
+docker run -d -it --network="dashnet" --name conda custom_conda /bin/bash
+docker run -d -it --network="dashnet" -p 8080:80 --name nginx nginx
+docker run -d -it --network="dashnet" --name ldap bitnami/openldap
+
+# Start containers
+docker start conda nginx ldap
+
+
+# ---
+# Setup gunicorn server in conda container
+# ---
+docker exec -it conda /bin/bash
+
+# Need to manually install gunicorn in the expose conda env...
+source activate explose_plot
+pip install gunicorn
+
+# configure gunicorn start script
+/root/bin/gunicorn_start
+
+
+# ---
+# Setup nginx container
+# ---
+docker cp containers/nginx/default.conf nginx:/etc/nginx/conf.d/
+docker exec nginx mkdir etc/nginx/location/
+docker cp containers/nginx/location_app.conf nginx:/etc/nginx/location/
+nginx -s reload
+
